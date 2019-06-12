208bf0178884bb0a2528c62d573b29828570b556
ishita pardeshi <ishitapardeshi0@gmail.com>
2019-06-12 12:45:28 +0530

    oo

---
+++ src/app/app.module.ts
@@ -0,0 +1,52 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { DemoComponent } from './demo/demo.component';
+
+import { SocialLoginModule } from 'angularx-social-login';
+import { AuthServiceConfig, GoogleLoginProvider, FacebookLoginProvider, LinkedInLoginProvider  } from 'angularx-social-login';
+
+const config = new AuthServiceConfig([
+  {
+    id: GoogleLoginProvider.PROVIDER_ID,
+    provider: new GoogleLoginProvider('624796833023-clhjgupm0pu6vgga7k5i5bsfp6qp6egh.apps.googleusercontent.com')
+  },
+  {
+    id: FacebookLoginProvider.PROVIDER_ID,
+    provider: new FacebookLoginProvider('418088032098662')
+  },
+  {
+    id: LinkedInLoginProvider.PROVIDER_ID,
+    provider: new LinkedInLoginProvider('78iqy5cu2e1fgr')
+  }
+]);
+
+export function provideConfig() {
+  return config;
+}
+
+@NgModule({
+  declarations: [
+    AppComponent,DemoComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpModule,
+    SocialLoginModule
+  ],
+  providers: [
+    {
+      provide: AuthServiceConfig,
+      useFactory: provideConfig
+    }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
